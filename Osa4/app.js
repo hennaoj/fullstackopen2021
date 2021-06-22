3dc07664fd3ce7f1a0a16a8aacf6485b0f27a310
henskumari <hennaojala99@gmail.com>
2021-06-22 17:31:39 +0300

    tehtävät 4.1-4.2

---
+++ Osa4/app.js
@@ -0,0 +1,19 @@
+const config = require('./utils/config')
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
