c9529814744711751ea0a6c80be1c4cf9d15a39d
henskumari <hennaojala99@gmail.com>
2021-08-20 14:36:43 +0300

    'tehtävät 9.10-9.11

--- Osa9/patientor_backend/src/index.ts
+++ Osa9/patientor_backend/src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosisRouter from './routes/diagnoses';
+import patientsRouter from './routes/patients';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -10,6 +13,9 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   return res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosisRouter);
+app.use('/api/patients', patientsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
